ac97c1b07bbaedba12f5aedaabde1c342cd7fc18
Raymond <raymond@philippinedev.com>
2021-06-16 18:45:46 +0800

    Initial Commit

---
+++ spec/card_spec.rb
@@ -0,0 +1,20 @@
+class Card
+  attr_reader :rank, :suit
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+end
+
+RSpec.describe Card do
+  before do
+    @card = Card.new('Ace', 'Spades')
+  end
+  it 'has a rank' do
+    expect(@card.rank).to eq('Ace')
+  end
+  it 'has a suit' do
+    expect(@card.suit).to eq('Spades')
+  end
+
+end
